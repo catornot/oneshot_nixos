afb2b8941ab7d2191e8eac18e4c097dae00e06ee
catornot <41955154+catornot@users.noreply.github.com>
2026-04-27 21:08:50 -0400

    remove extra deps

--- nix/deps.nix
+++ nix/deps.nix
@@ -2,54 +2,6 @@
 pkgs.symlinkJoin {
   name = "oneshot-env";
   paths = with pkgs; [
-    # mesa
-    # libGL
-    # libglvnd
-
-    # glib
-    # glib.out
-    # gtk3
-    # gtk2
-
-    # dbus
-    # zlib
-    # curl
-
-    # alsa-lib
-    # pulseaudio
-
-    # fontconfig
-    # freetype
-
-    # libxcrypt-legacy
-    # libgcrypt
-
-    # libsigcxx.out
-    # pixman
-    # zlib
-    # SDL2
-    # SDL2_image
-    # SDL2_ttf
-    # SDL_sound
-    # openal
-    # libvorbis
-    # physfs
-    # gtk3
-    # xfconf.out
-    # dbus-glib.out
-
-    # wayland
-
-    # libselinux.out
-    # util-linux.lib
-    # json-glib.out
-
-    # libx11
-    # libxext
-    # libxcursor
-    # libxrandr
-    # libxi
-
     libsigcxx
     pixman
     # zlib
